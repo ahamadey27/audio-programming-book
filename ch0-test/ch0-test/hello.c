9fbc69575caaaecfc0aa43bb17d944cb110e6941
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-12-20 17:41:24 -0500

    Update hello.c to add user input and sum calculation
    Modified hello.c to prompt for two integers, read user input, compute their sum, and display the result. Updated project file to suppress CRT warnings in Debug|x64 builds. Regenerated build artifacts to reflect these changes.

--- ch0-test/ch0-test/hello.c
+++ ch0-test/ch0-test/hello.c
@@ -1,7 +1,16 @@
-#include <stdio.h>
+#include <stdio.h> 
 
-int main(void)
+int main()
 {
-	printf("Hello, hammer!\n");
+	int a, b, c;
+	printf("\nPlease enter a number: ");
+	scanf("%d", &a); //user input stored as variable a
+
+	printf("\nPlease enter a second: ");
+	scanf("%d", &b); //user input stored as variable b
+
+	c = a+b;
+
+	printf("%d + %d = %d \n", a, b, c);
 	return 0;
 }
