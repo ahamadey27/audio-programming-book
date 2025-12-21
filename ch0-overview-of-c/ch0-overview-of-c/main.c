c20464cdfea2130ad111d8c5ed4cc348c35cd762
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-12-21 17:31:53 -0500

    Comment out printMajorScale and add bitwise shift example
    Commented out the printMajorScale() function call and its prototype in main.c. Added a commented-out code block demonstrating a bitwise left shift operation. No changes to program behavior; updated binaries due to recompilation.

--- ch0-overview-of-c/ch0-overview-of-c/main.c
+++ ch0-overview-of-c/ch0-overview-of-c/main.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 
 //void basic_input(void);
-void printMajorScale(void);
+//void printMajorScale(void);
 
 int main(void)
 {
 	//basic_input();
-	printMajorScale();
+	//printMajorScale();
+
+	/*int x = 8;
+	int newNum = x << 4;
+	printf("%d", newNum);*/
+
+
 	return 0;
 }
