f0556adb0b80ed01f4b27fd4a9d07b6bc5612e3b
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-12-21 18:42:41 -0500

    organized Source files and finished listing 1.2

--- ch1-programming-in-c/ch1-programming-in-c/midi2freq.c
+++ ch1-programming-in-c/ch1-programming-in-c/midi2freq.c
@@ -1,7 +1,25 @@
+/* Listing 1.2 Calculate frequency to MIDI exercise*/
 #include <stdio.h>
 #include "exercises.h"
+#include <math.h> //for pow() function
 
 void midi2freq()
 {
-	printf("midi2few script");
+	double semitone_ratio;
+	double c0; //for freq of MIDI note 0
+	double c5; //freq of middle c
+	double frequency; //whcih we want to find...
+	int midiNote; //...given this note
+
+	/*Calculate required numbers*/
+
+	semitone_ratio = pow(2, 1 / 12.0); //2 raised to the power of 1/12th to find semitone ratio of approx 1.0594631
+	c5 = 220.0 * pow(semitone_ratio, 3); // three semitones above low a of 220hz
+	c0 = c5 * pow(semitone_ratio, 5); //5 octaves above c0
+
+	/*Freq of a given midi note*/
+	midiNote = 73;
+	frequency = c0 * pow(semitone_ratio, midiNote);
+
+	printf("MIDI note %d has a frequency of %f\n", midiNote, frequency);
 }
